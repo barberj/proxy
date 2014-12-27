253be66c3f2262ccef12303f2bb13ef3f8bf188b
barberj <barber.justin@gmail.com>
2014-12-27 09:11:39 -0500

    make way for the internal account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ActiveRecord::Base
   has_many :users, inverse_of: :account
+
+  def internal?
+    self.id == 1
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   belongs_to :account, inverse_of: :users
+
+  def internal?
+    self.account_id == 1
+  end
 end
