0de2b9b13e63f3a7322020c3ae7445b28def5241
barberj <barber.justin@gmail.com>
2014-12-26 21:42:48 -0500

    start users

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+last_name "MyString"
+email "MyString"
+password_digest "MyString"
+last_login_at "2014-12-26 21:26:34"
+account_id 1
+  end
+
+end
