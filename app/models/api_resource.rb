7fcee9f13c25fa5d9f4d2fbb0fa099603442e5f2
barberj <barber.justin@gmail.com>
2014-12-28 08:25:08 -0500

    remove api prefix to keep things terse

--- app/models/api_resource.rb
+++
@@ -1,2 +0,0 @@
-class ApiResource < ActiveRecord::Base
-end
--- db/migrate/20141228021113_create_apis.rb
+++ db/migrate/20141228021113_create_apis.rb
@@ -2,8 +2,24 @@ class CreateApis < ActiveRecord::Migration
   def change
     create_table :apis do |t|
       t.string :name
+      t.string :install_url, null: false
+      t.string :uninstall_url, null: false
       t.boolean :is_active
       t.integer :account_id
+
+      t.timestamps null: false
+    end
+
+    create_table :resources do |t|
+      t.integer :api_id, null: false
+      t.string :name, null: false
+      t.string :search_url
+      t.string :created_url
+      t.string :updated_url
+      t.string :read_url
+      t.string :create_url
+      t.string :update_url
+      t.string :delete_url
 
       t.timestamps null: false
     end
