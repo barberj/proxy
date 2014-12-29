003c850830b0fd3e9d0bd4079fdb47a0edcfcee7
barberj <barber.justin@gmail.com>
2014-12-29 13:40:06 -0500

    default name spec green

--- app/models/installed_api.rb
+++ app/models/installed_api.rb
@@ -2,7 +2,7 @@ class InstalledApi < ActiveRecord::Base
   belongs_to :account, inverse_of: :installed_apis
   belongs_to :api, inverse_of: :installed_apis
 
-  before_create :generate_token
+  before_create :generate_token, :default_name
 
 private
 
@@ -10,5 +10,9 @@ private
     begin
       self.token = SecureRandom.hex
     end while self.class.exists?(:token => token)
+  end
+
+  def default_name
+    self.name ||= "My #{api.name}"
   end
 end
