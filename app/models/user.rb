2d579deb423b9ca6ca5c92eaaeb77d9e6b48c107
barberj <barber.justin@gmail.com>
2014-12-29 21:19:44 -0500

    better ordering of model validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   belongs_to :account, inverse_of: :users
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :password, length: { minimum: 6 }
+
+  has_secure_password
 
   before_save { self.email = email.downcase }
 
