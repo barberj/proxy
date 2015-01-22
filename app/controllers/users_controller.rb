e4ebc8f5fbf4404cf2b7716a65a06c6cff61b375
barberj <barber.justin@gmail.com>
2015-01-21 22:07:34 -0500

    redired to dashboard if already logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < AuthenticatedController
   skip_before_action :authenticate_user!, :only => [:new]
 
-  def new; end
+  def new
+    redirect_to dashboard_index_path if signed_in?
+  end
 end
